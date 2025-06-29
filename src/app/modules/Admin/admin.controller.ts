317a60ff0c87d834f1e8b2bd08b7044879ba5012
DevMohi <morphex72@gmail.com>
2025-06-29 12:02:42 +0600

    Part-1 Done & Overview - Response Format , Search Implementations and Pagination with reusable functions

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
+import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
+
+//This is done to prevent unwanted search Terms such as emaillsss , it will only pick the valid searchTerm or properties and that has values.
 
 const getAllAdmins = async (req: Request, res: Response) => {
   try {
-    const result = await AdminService.getAllAdminFromDB(req.query);
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+    console.log("options : ", options);
+    const result = await AdminService.getAllAdminFromDB(filters, options);
     res.status(200).json({
       success: true,
       message: "All Admin Data Retrieved Successfully",
@@ -12,7 +20,7 @@ const getAllAdmins = async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json({
       success: false,
-      message: err?.name || "Something went wrong",
+      message: (err as Error)?.name || "Something went wrong",
       error: err,
     });
   }
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,24 +1,12 @@
-import { Prisma, PrismaClient } from "../../../generated/prisma";
+import { Prisma } from "../../../generated/prisma";
+import { paginationHelper } from "../../../helpers/paginationHelper";
+import prisma from "../../../shared/prisma";
+import { adminSearchableFields } from "./admin.constant";
 
-const prisma = new PrismaClient();
-const getAllAdminFromDB = async (params: any) => {
+const getAllAdminFromDB = async (params: any, options: any) => {
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
-  const adminSearchableFields = ["name", "email"];
-  //Can also be written like this for line 22
-  // [
-  //       {
-  //         name: {
-  //           contains: params.searchTerm,
-  //           mode: "insensitive", //ignores alpahabet case problem
-  //         },
-  //       },
-  //       {
-  //         email: {
-  //           contains: params.searchTerm,
-  //           mode: "insensitive",
-  //         },
-  //       },
-  // ],
 
   if (params.searchTerm) {
     andConditions.push({
@@ -31,13 +19,37 @@ const getAllAdminFromDB = async (params: any) => {
     });
   }
 
-  console.dir(andConditions, { depth: "infinity" });
+  //Object theke array banano jai , specific field er upor filtering korte chaile aitai way
+  if (Object.keys(filterData.length > 0)) {
+    // And hoite hobe shob operation maintain korte hobe
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
+
+  // console.dir(andConditions, { depth: "infinity" });
   const whereConditions: Prisma.AdminWhereInput = {
     AND: andConditions,
   };
   const result = await prisma.admin.findMany({
     //where recieves object of array
     where: whereConditions,
+    skip,
+    take: Number(limit),
+    //jodi sortBy and sortOrder er value thake sort korbe ar naile default hobe sort hobe
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            //since aita object so third bracket e leka rakhe
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
   });
   return result;
 };
